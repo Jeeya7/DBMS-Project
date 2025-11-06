ad214471d66900e126580e62263d4d09287b9720
Jiya <pradhan.jiya7@gmail.com>
2025-11-06 13:05:55 -0800

    fix: Create pool instead of connection

--- db-connector.js
+++ db-connector.js
@@ -2,14 +2,14 @@
 require('dotenv').config();
 let mysql = require('mysql2')
 // Create a connection to the database
-let connection = mysql.createConnection({
+let pool = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
   waitForConnections: true,
   connectionLimit: 10,
-});
+}).promise(); 
 
 // Export it for use in our application
-module.exports = connection;
+module.exports = pool;
